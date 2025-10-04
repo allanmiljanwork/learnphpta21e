f7b03f31063843ba2f1dd40ae2097daf1969bd84
Allan <allan.miljan@tptlive.ee>
2025-10-04 10:41:12 +0300

    Add loop examples

--- index.php
+++ index.php
@@ -1,54 +1,37 @@
 <?php
-$num = 3;
-if($num > 5) {
-    var_dump('Bigger then 5');
-} else if ($num >= 5) {
-  var_dump('Equal to 5');
-} else {
-    var_dump('Smaller then 5');
+
+var_dump(PHP_INT_MAX); // annab max numbriline intergeri value PHP's
+for($i=0; $i<10; $i++){
+    var_dump($i);
 }
 
-$day = (int) date('N', strtotime('02.10.2025'));
-var_dump($day);
-
-if($day == 1) {
-    var_dump('Esmaspäev');
-} else if ($day == 2) {
-    var_dump('Teisipäev');
-} else if ($day == 3) {
-    var_dump('Kolmapäev');
-} else if ($day == 4) {
-    var_dump('Neljapäev');
-} else if ($day == 5) {
-    var_dump('Pidupäev');
-} else if ($day == 6) {
-    var_dump('Pidupäev');
-} else if ($day == 7) {
-    var_dump('Pühapäev');
-} else {
-    var_dump('Imelik päev');
+for($i=10; $i>0; $i--){
+    var_dump($i);
 }
 
-switch($day) {
-    case 1:
-        var_dump('Esmaspäev');
-        break;
-    case 2:
-        var_dump('Teisipäev');
-        break;
-    case 3:
-        var_dump('Kolmapäev');
-        break;
-    case 4:
-        var_dump('Neljapäev');
-        break;
-    case 5:
-    case 6:
-        var_dump('Pidupäev');
-        break;
-    case 7:
-        var_dump('Pühapäev');
-        break;
-        default:
-        var_dump('Imelik päev');
+for($i=1; $i<1_000_000; $i*=2){
+    var_dump($i);
+}
+
+$current = (int) date("s");
+
+$i = 10;
+
+while($i<10){
+    var_dump('While');
+}
+
+do {
+    var_dump('Do While');
+}   while($i<10);
+
+$array = ['apples', 'pears', 'oranges'];
+
+foreach($array as $value):
+    echo $value . " ";
+endforeach;
+
+
+foreach($array as $key => $value) {
+    var_dump($key, $value);
 }
